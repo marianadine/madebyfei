c8d4165769b53631f862efbd39a4884a415e0927
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-07-07 11:15:41 +0800

    mobile responsive design warning message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import WebController from './controller/WebController';
 function App() {
   return (
     <div className="App">
+      <div className="mobile-warning">
+        This website is recommended to be used on 100% scale desktop.
+      </div>
+
       <WebController />
     </div>
   );
