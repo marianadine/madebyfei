7518e18626e697300757e60b963cd45a325f4a98
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-07-07 11:48:44 +0800

    update WebController

--- src/controller/WebController.jsx
+++ src/controller/WebController.jsx
@@ -3,6 +3,7 @@ import Home from "../components/Home";
 import About from "../components/About";
 import Designs from "../components/Designs";
 import Navbar from "../components/Navbar";
+import Contact from "../components/Contact";
 
 function WebController() {
   return (
@@ -12,6 +13,7 @@ function WebController() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/designs" element={<Designs />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
