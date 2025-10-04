a7afcb8474273c4c0aac062864f1633c106998df
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-10-04 11:40:27 +0800

    comment note

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import WebController from './controller/WebController';
 function App() {
   return (
     <div className="App">
-      <div className="mobile-warning">
+      {/* <div className="mobile-warning">
         This website is recommended to be used on 100% scale desktop.
-      </div>
+      </div> */}
 
       <WebController />
     </div>
